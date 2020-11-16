e6895eec9b9afcc451670f03737c1d311d3b8984
james <jguest777@gmail.com>
2020-11-16 13:34:53 -0500

    added spring parallax API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useSpring, animated } from 'react-spring';
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+
 const App = () => {
+  let parallax;
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <Parallax pages={4} ref={(ref) => (parallax = ref)}>
+      <ParallaxLayer offset={0.1} speed={0}>
+        <div style={{ backgroundColor: 'blue' }}>One</div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.2} speed={1}>
+        <div style={{ backgroundColor: 'red' }}>Two</div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.3} speed={2}>
+        <div style={{ backgroundColor: 'orange' }}>Three</div>
+      </ParallaxLayer>
+      <ParallaxLayer offset={0.4} speed={3}>
+        <div style={{ backgroundColor: 'pink' }}>Four</div>
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
